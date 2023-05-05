b46e647b5ad46359b7bf991c2a2da6a6d9f9aa2f
ManishaMagdum <manisha.khot911@gmail.com>
2023-05-05 17:48:31 +0530

    assignment done
    assignment done

---
+++ 01_variable_assign.js
@@ -0,0 +1,15 @@
+
+
+
+var myFullName;
+myFullName = "Manisha Magdum";
+console.log(myFullName);
+myFullName = "Manisha Pravin Magdum";
+console.log(myFullName);
+var myLove;
+myLove = "I love only JS";
+console.log(myLove);
+var form;
+form = "Manisha Magdum 25 Gadhinglaj,Kolhapur,416502 Female Married";
+console.log(form);
+
---
+++ hello.js
@@ -0,0 +1,19 @@
+
+
+
+
+console.log("Manisha Magdum..");
+console.log("Gadhinglaj,Kolhapur");
+var firstName = "Manisha";
+console.log(firstName)
+var age; // Variable Declaration 
+age = "22";  // Variable Initialization
+console.log(age);
+
+var holder; // Variable Declaration
+console.log(holder); // undefined
+holder = "My Spec"; // Variable Initialization
+console.log(holder);
+
+holder = "Sachin Spec"; // Variable update
+console.log(holder); //
