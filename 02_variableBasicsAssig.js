246a82cfc21089d8df600bd302948bdb6dd4bd74
ManishaMagdum <manisha.khot911@gmail.com>
2023-05-09 06:48:36 +0530

    assignment Done
    assignment Done

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,75 @@
+
+
+console.log("-----number type------");
+var mobileNumber;
+mobileNumber = 1234567891;
+console.log("Mobile Number:",mobileNumber);
+var typeofmobileNumber = typeof mobileNumber;
+console.log("Type:",typeofmobileNumber);
+var accountNumber;
+accountNumber = 23785651878;
+console.log("Account Number:",accountNumber);
+var typeofaccountNumber = typeof accountNumber;
+console.log("Type:",typeofaccountNumber);
+var pincode;
+pincode = 416502;
+console.log("Pincode:",pincode);
+var typeofpincode = typeof pincode;
+console.log("Type",typeofpincode);
+
+console.log("-----String Type-----");
+
+var favColour;
+favColour = "Red";
+console.log("Fav Colour:",favColour);
+var typeoffavColour = typeof favColour;
+console.log("Type",typeoffavColour);
+var favFruit;
+favFruit ="Orange";
+console.log("Fav Fruit:",favFruit);
+var typeoffavFruit = typeof favFruit;
+console.log("Type",typeoffavFruit);
+var favSubject;
+favSubject ="Maths";
+console.log("Fav Subject:",favSubject);
+var typeoffavSubject = typeof favSubject;
+console.log("Type",typeoffavSubject);
+
+console.log("-----Boolean Type----");
+var islikeAngular = true;
+console.log(islikeAngular);
+ var typeofislikeAngular = typeof islikeAngular;
+ console.log("Type",typeofislikeAngular);
+ var isknownJava = false;
+ console.log(isknownJava);
+ var typeofisknownJava = typeof isknownJava;
+ console.log("Type",typeofisknownJava);
+ var islikeCodding = true;
+ console.log(islikeCodding);
+ var typeofislikeCodding = typeof islikeCodding;
+ console.log("Type",typeofislikeCodding);
+
+ console.log("------Diffenrence Between Null and Undefined-----");
+
+ console.log("Undefoned:The Variable which value is not declared");
+ console.log("typeof undefined is undefined");
+ console.log("Null:The variable which value is assigned as null");
+ console.log("typeof null is object");
+
+ console.log("Example of Null and Undefined");
+ var Name;
+ console.log("Name:",Name);
+ var typeofName = typeof Name;
+ console.log("Type",typeofName);
+
+ var birthDate;
+ birthDate = null;
+ console.log("Birth Date", birthDate);
+ var typeofbirthDate = typeof birthDate;
+ console.log("Type",typeofbirthDate);
+
+
+
+
+
+
---
+++ 03_typeOf.js
@@ -0,0 +1,29 @@
+var bankName;
+console.log("Bank Name",bankName);
+bankName = "SBI Bank";
+console.log("Bank Name",bankName);
+var typeofbankName = typeof bankName;
+console.log("Type",typeofbankName);
+var marks;
+console.log("Marks:",marks);
+var marks;
+marks = "90%";
+console.log("Marks:",marks);
+var typeofmarks = typeof marks;
+console.log("type",typeofmarks);
+var isWorking;
+isWorking = true;
+console.log("isWorking",isWorking);
+var typeofisWorking = typeof isWorking;
+console.log("Type:",typeofisWorking);
+var totalCount;
+totalCount = "Hundred and seven";
+console.log("Total Count:",totalCount);
+var typeoftotalCount = typeof totalCount;
+console.log("Type:",typeoftotalCount);
+var totalCount = 107
+console.log("Total Count:",totalCount);
+var typeoftotalCount = typeof totalCount;
+console.log("Type:",typeoftotalCount);
+
+
