9cd027efe73b793541cd0364a862a8f0fd205595
ManishaMagdum <manisha.khot911@gmail.com>
2023-05-15 13:17:03 +0530

    Operators assignment done
    Operators assignment done

---
+++ 05_operatorA.js
@@ -0,0 +1,33 @@
+console.log(`1.Length of word and its square`);
+function sqaureOfWordLength(str){
+    var result = str.split(" ");
+
+var length = result.length;
+console.log(`Length of given string is: ${length}`);
+var a = length;
+var b = length;
+var sqaure = a*b;
+console.log(`Square Of length of given string is: ${sqaure}`);
+}
+sqaureOfWordLength("JavaScript");
+sqaureOfWordLength("Google Chrome");
+sqaureOfWordLength("Developer Smart");
+
+console.log(`2.Division and multiplication`);
+function data(){
+  var str ="I am Angular Developer";
+  var length = str.length;
+  var word = str.split(" ");
+  var lengthWord = word.length;
+  var a = length;
+  var b = lengthWord;
+  var Division = a/b;
+  console.log(`Division of string length to word length is: ${Division}`);
+  var multiplication = a*b;
+  console.log(`Multiplication of string length to word length is: ${multiplication}`);
+}
+
+
+data("I am Angular Developer");
+
+
