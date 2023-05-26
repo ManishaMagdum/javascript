fba9a86c2a553c636ea2a5e4c96cde863e6f2a10
ManishaMagdum <manisha.khot911@gmail.com>
2023-05-26 09:02:46 +0530

    while loop
    while loop

---
+++ 06_while_Assignment01.js
@@ -0,0 +1,57 @@
+console.log(`1)WAP to print numbers from 5 to 15 `);
+let i = 5;
+while (i<=15) {
+  console.log(i);
+  i = i +1;  
+}
+console.log(`------------------------------------------`);
+console.log(`2)WAP to print numbers from 50 to 40 `);
+let j= 50;
+while (j>=40) {
+   console.log(j);
+   j=j-1; 
+}
+console.log(`------------------------------------------`);
+console.log(`3)WAP to print firts 15 odd numbers `);
+let k=0;
+while (k<=15) {
+    if (k%2!=0) {
+        console.log(k);
+    }
+    k++;
+}
+console.log(`------------------------------------------`);
+console.log(`4)WAP to print firts 10 even numbers `);
+let l=0;
+while (l<=10) {
+    if (l%2==0) {
+        console.log(l);
+    }
+    l++;
+}
+console.log(`------------------------------------------`);
+console.log(`5)WAP to print 5 like 5 10 15.............50 `);
+let m=5;
+while (m<=50) {
+    console.log(m);
+    m= m+5;
+}
+console.log(`------------------------------------------`);
+console.log(`6)WAP to print table 10 20.............100 `);
+let n=10;
+while (n<=100) {
+    console.log(n);
+    n= n+10;    
+}
+console.log(`------------------------------------------`);
+console.log(`7)WAP to print table of 10 in reverse order like  100 90 80 ...........10`);
+let o=100;
+while (o>=10) {
+    console.log(o);
+    o= o-10;
+}
+
+//let i=0; // initialization i = 1   2  3
+//while (i<=10) { // condition 0<=10
+   //console.log(i); // 0  1 2
+  // i = i + 1;
